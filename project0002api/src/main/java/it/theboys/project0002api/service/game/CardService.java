2e4251aea88c32447fcfd49aab041b5eb4a4892c
charopevez <charopevez@gmail.com>
2021-10-26 00:03:09 +0900

    added ObjectUtils

--- project0002api/src/main/java/it/theboys/project0002api/service/game/CardService.java
+++ project0002api/src/main/java/it/theboys/project0002api/service/game/CardService.java
@@ -4,13 +4,10 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import it.theboys.project0002api.dto.base.SetListDto;
-import it.theboys.project0002api.dto.http.request.CardListDTO;
 import it.theboys.project0002api.exception.database.CardSetCollectionException;
 import it.theboys.project0002api.model.base.CardSet;
-import it.theboys.project0002api.repository.CahCardRepository;
 import it.theboys.project0002api.repository.CardSetRepository;
-import lombok.AllArgsConstructor;
-import org.apache.catalina.mapper.Mapper;
+import it.theboys.project0002api.utils.ObjectUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -25,10 +22,11 @@ public class CardService {
     private CardSetRepository setRepo;
 
     public List<CardSet> getCardsBySet(String gameName) {
-        switch (gameName){
+        switch (gameName) {
             case "cah":
                 return setRepo.findAll();
-                default: return null;
+            default:
+                return null;
         }
     }
 
@@ -37,13 +35,13 @@ public class CardService {
         ObjectMapper mapper = new ObjectMapper();
         mapper.enable(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY);
         SetListDto setList = mapper.readValue(json, SetListDto.class);
-        Map<String, List<CardSet>> responseBody= new HashMap<>();
+        Map<String, List<CardSet>> responseBody = new HashMap<>();
         List<CardSet> added = new ArrayList<>();
         List<CardSet> existed = new ArrayList<>();
         setList.getSetList().forEach(
-                (set)->{
-                    Optional<CardSet> setInDB=setRepo.findCardSetByGameNameAndSetName(gameName,set.getSetName());
-                    if (setInDB.isPresent()){
+                (set) -> {
+                    Optional<CardSet> setInDB = setRepo.findCardSetByGameNameAndSetName(gameName, set.getSetName());
+                    if (setInDB.isPresent()) {
                         existed.add(set);
                     } else {
                         set.setGameName(gameName);
@@ -52,49 +50,83 @@ public class CardService {
                     }
                 }
         );
-        responseBody.put("Successfully added to db",added);
-        responseBody.put("Already exists in db",existed);
+        responseBody.put("Success", added);
+        responseBody.put("Failure", existed);
         return responseBody;
     }
+
     public List<CardSet> getSets(String gameName) {
-        switch (gameName){
+        switch (gameName) {
             case "cah":
                 return setRepo.findAll();
-            default: return null;
+            default:
+                return null;
         }
     }
 
     public CardSet findSetById(String id) throws CardSetCollectionException {
 
-        Optional<CardSet> set=setRepo.findById(id);
+        Optional<CardSet> set = setRepo.findById(id);
         if (set.isPresent()) {
             return set.get();
-
         } else {
             throw new CardSetCollectionException(CardSetCollectionException.NotFoundException(id));
-            }
+        }
     }
 
     // TODO add user yo setAuthor
-    public CardSet updateSet(String id, Map<String, ?> setData) {
-        Optional<CardSet> setToUpdate=setRepo.findById(id);
-        if (setToUpdate.isPresent()){
-            CardSet setStoredData=setToUpdate.get();
-            setData.forEach((key, value)->{
-                switch (key){
-                    case "gameName": setStoredData.setGameName(value.toString());break;
-                    case "setName": setStoredData.setSetName(value.toString());break;
-                    case "setDescription": setStoredData.setSetDescription(value.toString());break;
-                    case "isCustom" : setStoredData.setPrivate((boolean) value);break;
-                }
-            });
-            setStoredData.setUpdatedAt(Instant.now().toEpochMilli());
-            return setRepo.save(setStoredData);
+    public CardSet updateSet(String id, CardSet newSetData, String gameName) throws CardSetCollectionException {
+        Optional<CardSet> setById = setRepo.findById(id);
+        // check if set with @param id is exists
+        if (setById.isEmpty()) {
+            //if not try add set to db
+            newSetData.setGameName(gameName);
+            newSetData.setAddedAt(Instant.now().toEpochMilli());
+            return setRepo.save(newSetData);
         }
-        return null;
+        //check if set with new name exist in this @param gameName
+        Optional<CardSet> setByName = setRepo.findCardSetByGameNameAndSetName(gameName, newSetData.getSetName());
+        if (setByName.isPresent() && !setByName.get().getId().equals(id)) {
+            throw new CardSetCollectionException(CardSetCollectionException.AlreadyExistException(gameName, newSetData.getSetName()));
+        }
+        CardSet updatingSet = setById.get();
+        // get all functions from class
+//        Method[] methods = updatingSet.getClass().getMethods();
+//
+//        for(Method fromMethod: methods){
+//            if(fromMethod.getDeclaringClass().equals(updatingSet.getClass())
+//                    && fromMethod.getName().startsWith("get")){
+//
+//                String fromName = fromMethod.getName();
+//                String toName = fromName.replace("get", "set");
+//
+//                try {
+//                    Method toMetod = updatingSet.getClass().getMethod(toName, fromMethod.getReturnType());
+//                    Object value = fromMethod.invoke(newSetData, (Object[])null);
+//                    if(value != null){
+//                        if (value instanceof String && value.toString().length() > 0) {
+//                            toMetod.invoke(updatingSet, value);
+//                        };
+//                        if (value instanceof Integer && (int)value > 0) {
+//                            toMetod.invoke(updatingSet, value);
+//                        };
+//                    }
+//                } catch (Exception e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }
+
+//        updatedSet.setSetExpansion(newSetData.getSetExpansion());
+//        updatedSet.setSetDescription(newSetData.getSetDescription());
+//        updatedSet.setPrivate(newSetData.isPrivate());
+//        updatedSet.setSetName(newSetData.getSetName());
+//        updatedSet.setSetAgeRestrictions(newSetData.getSetAgeRestrictions());
+//        updatedSet.setUpdatedAt(Instant.now().toEpochMilli());
+        return setRepo.save(new ObjectUtils<CardSet>().updateObjectFromObject(updatingSet,newSetData));
     }
 
-    public boolean deleteSet(String id) throws CardSetCollectionException{
+    public boolean deleteSet(String id) throws CardSetCollectionException {
         try {
             setRepo.deleteById(id);
             return true;
