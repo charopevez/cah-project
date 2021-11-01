f38cf193e991eab94530e9466c93e7194cdde459
charopevez <charopevez@gmail.com>
2021-11-01 12:17:35 +0900

    commit befor pull

--- project0002api/src/main/java/it/theboys/project0002api/controller/GameController.java
+++ project0002api/src/main/java/it/theboys/project0002api/controller/GameController.java
@@ -1,4 +1,26 @@
 package it.theboys.project0002api.controller;
 
+import org.springframework.http.ResponseEntity;
+
 public class GameController {
+
+    public ResponseEntity<?> launchGameServer(String user, String settings){
+        service.launchGameServer;
+    }
+
+    public ResponseEntity<?> joinGameServer(String user, String password, String gameServerId){
+        service.joinGameServer;
+    }
+    public ResponseEntity<?> leaveGameServer(String user, String gameServerId){
+        service.leaveGameServer;
+    }
+    public ResponseEntity<?> terminateGameServer(String user, String gameServerId){
+        service.terminateGameServer;
+    }
+    public ResponseEntity<?> terminateGameServer(String user, String gameServerId){
+        service.messageToServer;
+    }
+
+
+
 }
--- project0002api/src/main/java/it/theboys/project0002api/service/GameMainframeService.java
+++ project0002api/src/main/java/it/theboys/project0002api/service/GameMainframeService.java
@@ -5,12 +5,6 @@ import java.util.List;
 import it.theboys.project0002api.model.cah.GameEngine;
 
 public class GameMainframeService {
-    private List<GameEngine> gameList;
 
-    // public startGame(setting){
-    //     GE newGame= Gm();
-    //     ng.start;
-    //     gameList.add();
-    // }
     
 }
